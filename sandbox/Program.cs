bbfdea81385a2528f758d1339571f8ceae450807
Herbert <Herbert.hawes.iii@gmail.com>
2018-04-30 10:07:17 -0400

    AR File Update Method
    File correction code for an AR file.

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,30 @@ namespace sandbox
     {
         static void Main(string[] args)
         {
-            Stats.Solution2c();
+            //Stats.Solution2c();
 
             string s = "";
+        }
+
+        static void FixFile(string input, string output)
+        {
+            string[] readText = File.ReadAllLines(input);
+            for(int i = 0; i < readText.Length-1; i++)
+            {
+                if (readText[i].Substring(56, 1) == " ")
+                {
+                    readText[i] = readText[i].Substring(0, 56) + "-" + readText[i].Substring(57);
+                }
+                else if(readText[i].Substring(56, 1) == "*")
+                {
+                    readText[i] = readText[i].Substring(0, 56) + " " + readText[i].Substring(57);
+                }
+                else
+                {
+                    //Unreachable code
+                } 
+            }
+            File.WriteAllLines(output, readText);
         }
     }
 }
